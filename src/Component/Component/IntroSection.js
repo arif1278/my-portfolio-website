84a76d9f515c5fde9144249ebbb4d0f40bfd175c
arif1278 <manabethreeplusone@gmail.com>
2023-08-20 21:27:08 +0600

    particles and typer

--- src/Component/Component/IntroSection.js
+++ src/Component/Component/IntroSection.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './ItroSection.css';
+import Particles from "react-particles";
+import { useCallback } from "react";
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
 import Github from '../../assets/git hub image.png';
 import linkedin from '../../assets/linkedin image.png';
 import Instragram from '../../assets/instragram image.png';
@@ -11,22 +15,117 @@ import { themeContext } from './Toggle/Context';
 import { useContext } from 'react';
 import FlotingDiv from './FlotingDiv/FlotingDiv';
 import {motion} from 'framer-motion'
+import {Typewriter} from 'react-simple-typewriter'
 
 
 
 const IntroSection = () => {
+
+    const particlesInit = useCallback(async engine => {
+        console.log(engine);
+        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async container => {
+        await console.log(container);
+    }, []);
+
     const transition={duration:2,type:'spring'}
     const theme=useContext(themeContext);
     const darkMode=theme.state.darkMode;
     return (
+        
         <div className='intro'>
+            <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+                fpsLimit: 800,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
             <div className='i-left'>
             <div className='i-name'>
-                <span style={{color:darkMode? 'white':''}}>Hey! I'm</span>
-                <span>Arif Hossain </span>
-                <span>Mern Developer with 
-                     high level of experience in<br></br>
-                      React ,Web Design and Development</span>
+                <span style={{color:darkMode? 'white':''}}>Hey! I'm </span>
+                <span>Arif Hossain</span>
+
+                <span style={{ color:darkMode? 'white':'', fontWeight: 'bold' }}> I'm a {' '}
+          {/* Style will be inherited from the parent element */}
+           <Typewriter
+            words={['Mern Stack Developer', ' Web Designer', 'Photographer', 'Lazy!']}
+            loop={Infinity}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
             </div>
             <button className=' button i-button'>
             Hire me
